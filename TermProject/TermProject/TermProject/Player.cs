ece0cbca2d66e9b3b2899db8a3ebe021ebeca2fe
BURCHARD JOSIAH M <jmburchard1s@semo.edu>
2015-11-03 09:37:07 -0600

    Later player movement

--- TermProject/TermProject/TermProject/Player.cs
+++ TermProject/TermProject/TermProject/Player.cs
@@ -16,19 +16,42 @@ namespace TermProject
 
         }
 
+        private const int MAX_SPEED = 8;
+
+        private enum Direction
+        {
+            Left = -1,
+            Right = 1
+        }
+
         public new void Move(Keys[] keys)
         {
             bool PlayerIsOnGround = true; // needs to be implemented
 
-            if (PlayerIsOnGround && (keys.Contains(Keys.Space) || keys.Contains(Keys.Up)))
+            if (PlayerIsOnGround && (keys.Contains(Keys.Space) || keys.Contains(Keys.Up) || keys.Contains(Keys.W)))
             {
                 Jump();
+            }
+
+            if (keys.Contains(Keys.Right) || keys.Contains(Keys.D))
+            {
+                Move(Direction.Right);
+            }
+
+            if (keys.Contains(Keys.Left) || keys.Contains(Keys.A))
+            {
+                Move(Direction.Left);
             }
         }
 
         public void Jump()
         {
             this.velocity.Y = -10;
+        }
+
+        private void Move(Direction direction)
+        {
+            this.velocity.X = Math.Max(MAX_SPEED * -1, Math.Min(this.velocity.X + (int)direction, MAX_SPEED));
         }
     }
 }
