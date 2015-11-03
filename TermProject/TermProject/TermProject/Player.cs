671c08bae4eae6fb50d498dbbab5fad02781b3ed
SEBASTIAN STEVEN R <srsebastian1s@semo.edu>
2015-11-03 09:38:53 -0600

    Basic gravity code added

--- TermProject/TermProject/TermProject/Player.cs
+++ TermProject/TermProject/TermProject/Player.cs
@@ -9,6 +9,8 @@ namespace TermProject
 {
     class Player : AnimatedObject
     {
+        Rectangle groundChecker;
+
         public Player(Texture2D loadedTexture, Vector2 position, int frameCount, int framesPerSec)
             : base(loadedTexture, position, 0f, 1f, 1f, frameCount, framesPerSec)
         {
@@ -18,6 +20,18 @@ namespace TermProject
         public void Jump()
         {
             this.velocity.Y = -10;
+        }
+
+        public void FallOntoGround(List<GameObject> levelObjects)
+        {
+            for (int i = 0; i < levelObjects.Count; i++)
+            {
+                if(groundChecker.Intersects(levelObjects[i].Rectangle)
+                {
+                    if((levelObjects[i] is SemiSolidTile || levelObjects[i] is SolidTile) && this.velocity.Y > 0)
+                        this.isOnGround = true;
+                }
+            }
         }
     }
 }
