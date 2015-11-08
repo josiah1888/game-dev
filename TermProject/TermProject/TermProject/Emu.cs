238cbb5c29c737df590cb986f549aa8cc0f2228a
Josiah Burchard <josiah.burchard@gmail.com>
2015-11-07 20:31:35 -0600

    Adding new files after refactor

---
+++ TermProject/TermProject/TermProject/Emu.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TermProject
+{
+    public class Emu : Enemy
+    {
+        public Emu(ContentManager content, Vector2 position)
+            : base(content.Load<Texture2D>("sprites/place-holder"), position, 1, 1, Emu.Ai)
+        {
+
+        }
+
+        public static Action<Enemy> Ai
+        {
+            get
+            {
+                return (Enemy Emu) =>
+                {
+                    // do ai logic using this instance of Emu
+
+                    //if (Emu.getState() == Enemy.EnemyState.Idle)
+                    //{
+                    //    Emu.velocity.Y = -10;
+                    //    //Emu.velocity.X = Emu.MAX_SPEED * Emu.
+                    //}
+                };
+            }
+        }
+    }
+}
---
+++ TermProject/TermProject/TermProject/Frog.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TermProject
+{
+    public class Frog : Enemy
+    {
+        public Frog(ContentManager content, Vector2 position)
+            : base(content.Load<Texture2D>("sprites/place-holder"), position, 1, 1, Frog.Ai)
+        {
+
+        }
+
+        public static Action<Enemy> Ai
+        {
+            get
+            {
+                return (Enemy frog) =>
+                {
+                    // do ai logic using this instance of frog
+
+                    //if (frog.getState() == Enemy.EnemyState.Idle)
+                    //{
+                    //    frog.velocity.Y = -10;
+                    //    //frog.velocity.X = frog.MAX_SPEED * frog.
+                    //}
+                };
+            }
+        }
+    }
+}
