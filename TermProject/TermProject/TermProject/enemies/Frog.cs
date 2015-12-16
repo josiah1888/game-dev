ceb12c7c916dc40f565725180c0a04896ddc1e27
Josiah Burchard <josiah.burchard@gmail.com>
2015-12-16 16:23:15 -0600

    Refactored frog ai

--- TermProject/TermProject/TermProject/enemies/Frog.cs
+++ TermProject/TermProject/TermProject/enemies/Frog.cs
@@ -19,7 +19,6 @@ namespace TermProject
         {
             this.IdleSprite = content.Load<Texture2D>("sprites/frog-idle");
             this.AttackSprite = content.Load<Texture2D>("sprites/frog-jump");
-            this.TimePerFrame = 200f;
         }
 
         private static Action<Enemy, double> Ai
@@ -29,53 +28,57 @@ namespace TermProject
                 return (Enemy enemyFrog, double elapsed) =>
                 {
                     Frog frog = (Frog)enemyFrog;
+                    bool isOnGround = frog.IsOnGround();
 
-                    if (Vector2.Distance(frog.Position, frog.Target.Position) > THRESHHOLD || frog.Target.IsOnGround() || (frog.State == EnemyState.Idle && !frog.IsOnGround()))
+                    frog.State = Vector2.Distance(frog.Position, frog.Target.Position) > THRESHHOLD || frog.Target.IsOnGround() || (frog.State == EnemyState.Idle && !isOnGround)
+                        ? EnemyState.Idle
+                        : EnemyState.Attack;
+
+                    switch (frog.State)
                     {
-                        frog.State = EnemyState.Idle;
-                    }
-                    else
-                    {
-                        frog.State = EnemyState.Attack;
-                    }
-
-                    if (frog.State == EnemyState.Idle)
-                    {
-                        if (frog.IsOnGround())
-                        {
-                            frog.Velocity.X = 0;
-                            if (frog.Timer.IsTimeYet(elapsed, JUMP_DELAY_TIME))
-                            {
-                                frog.Velocity.Y = -10;
-                                frog.Direction = (new List<Directions>() { Directions.Left, Directions.Right }[Rand.Next(2)]);
-                            }
-                        }
-                        else
-                        {
-                            frog.Velocity.X = (MAX_SPEED / 2) * frog.Direction.GetLateralDirectionSign();
-                        }
-                    }
-
-                    else if (frog.State == EnemyState.Attack)
-                    {
-                        Vector2 closingVelocity = Vector2.Zero;
-                        closingVelocity.X = frog.Target.Velocity.X - frog.Velocity.X;
-
-                        Vector2 closingRange = Vector2.Zero;
-                        closingRange.X = frog.Target.Position.X - frog.Position.X;
-
-                        Vector2 closingTime = new Vector2(Math.Abs(closingRange.X) / Math.Abs(closingVelocity.X), 0);
-
-                        Vector2 frogFuturePosition = frog.Target.Position + (frog.Target.Velocity * closingTime);
-                        float angle = frog.GetAngle(frog.Position, frogFuturePosition);
-
-                        if (frog.IsOnGround())
-                            frog.Velocity.Y = -10;
-
-                        if (!frog.IsOnGround())
-                            frog.Velocity.X = (float)Math.Cos(angle) * MAX_SPEED / 1.5f;
+                        case EnemyState.Idle:
+                            Ai_Idle(frog, elapsed);
+                            break;
+                        case EnemyState.Attack:
+                            Ai_Attack(frog);
+                            break;
                     }
                 };
+            }
+        }
+
+        private static void Ai_Idle(Frog frog, double elapsed)
+        {
+            if (frog.IsOnGround())
+            {
+                frog.Velocity.X = 0;
+                if (frog.Timer.IsTimeYet(elapsed, JUMP_DELAY_TIME))
+                {
+                    frog.Velocity.Y = -10;
+                    frog.Direction = (new List<Directions>() { Directions.Left, Directions.Right }[Rand.Next(2)]);
+                }
+            }
+            else
+            {
+                frog.Velocity.X = (MAX_SPEED / 2) * frog.Direction.GetLateralDirectionSign();
+            }
+        }
+
+        private static void Ai_Attack(Frog frog)
+        {
+            Vector2 closingVelocity = new Vector2(frog.Target.Velocity.X - frog.Velocity.X, 0);
+            Vector2 closingRange = new Vector2(frog.Target.Position.X - frog.Position.X, 0);
+            Vector2 closingTime = new Vector2(Math.Abs(closingRange.X) / Math.Abs(closingVelocity.X), 0);
+            Vector2 frogFuturePosition = frog.Target.Position + (frog.Target.Velocity * closingTime);
+            float angle = frog.GetAngle(frog.Position, frogFuturePosition);
+
+            if (frog.IsOnGround())
+            {
+                frog.Velocity.Y = -10;
+            }
+            else
+            {
+                frog.Velocity.X = (float)Math.Cos(angle) * MAX_SPEED / 1.5f;
             }
         }
     }
